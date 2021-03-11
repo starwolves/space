318a8d334177b2e7fae5ea23e83b209d00722356
Nikita <nikitaab98@gmail.com>
2021-03-11 18:48:14 +0100

    Basic Bevy & Rapier3D setup.
    ECS framework, check. 3D Physics framework, check.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use bevy::prelude::*;
+use bevy_rapier3d::physics::{RapierPhysicsPlugin};
+use bevy_rapier3d::rapier::na::Vector;
 
 fn main() {
-    App::build().run();
+    App::build()
+    .add_plugin(RapierPhysicsPlugin)
+    .add_startup_system(launch_server.system())
+    .run();
+}
+
+fn launch_server(commands: &mut Commands) {
+
+
+
 }
