78ac90f65030a4bda41ee2d143b4ed3f8c2fe714
Nikita <nikitaab98@gmail.com>
2021-03-12 23:08:24 +0100

    Build map of static cubes from json
    Ball drops very slowly now for some reason.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use bevy::{app, prelude::*};
-use bevy_rapier3d::{na::Point3, physics::{RapierPhysicsPlugin}};
-use bevy_rapier3d::rapier::na::Vector;
+use std::fs;
 
-use bevy_rapier3d::rapier::dynamics::RigidBodyBuilder;
-use bevy_rapier3d::rapier::dynamics::RigidBody;
+use bevy::{prelude::*};
 
+use bevy_rapier3d::{
+    physics::{
+        RapierPhysicsPlugin,
+        RigidBodyHandleComponent
+    },
+    rapier::{
+        dynamics::{
+            RigidBodyBuilder,
+            RigidBodySet
+        },
+        geometry::{
+            ColliderBuilder
+        }
+    }
+};
 
-use bevy_rapier3d::rapier::geometry::ColliderBuilder;
-
-use bevy_rapier3d::physics::PhysicsInterpolationComponent;
-use bevy_rapier3d::rapier::dynamics::RigidBodySet;
-use bevy_rapier3d::physics::RigidBodyHandleComponent;
+use serde::{Deserialize};
 
 struct PhysicsDynamicRigidBodyComponent;
 
@@ -23,18 +31,37 @@ fn main() {
         .run();
 }
 
+const DEFAULT_MAP_LOCATION : &str = "content\\maps\\bullseye\\main.json";
+
+#[derive(Deserialize)]
+struct CellData {
+    id: String,
+    item: i64,
+    orientation: i64
+}
+
 fn launch_server(commands: &mut Commands) {
 
-    // Spawn floor and then a cube up high thatll fall down and hit it.
+    // Load map json data
 
+    let current_map_main_raw_json : String = fs::read_to_string(&DEFAULT_MAP_LOCATION).expect("main.rs launch_server() Error reading map main.json file from drive.");
+    let current_map_main_data : Vec<CellData> = serde_json::from_str(&current_map_main_raw_json).expect("main.rs launch_server() Error parsing map main.json String.");
+    
+    for cell_data in current_map_main_data.iter() {
+        
+        let cell_id = json_vec3_to_vec3(&cell_data.id);
+
+        let world_position = cell_id_to_world(cell_id);
+
+        commands.spawn((
+            RigidBodyBuilder::new_static().translation(world_position.x, world_position.y, world_position.z),
+            ColliderBuilder::cuboid(1., 1., 1.),
+        ));
+
+    }
+    
     commands.spawn((
-        RigidBodyBuilder::new_static().translation(0., 0., 0.),
-        ColliderBuilder::cuboid(64., 0., 64.),
-    ));
-
-
-    commands.spawn((
-        RigidBodyBuilder::new_dynamic().translation(0., 100., 0.),
+        RigidBodyBuilder::new_dynamic().translation(0., 10., 0.),
         ColliderBuilder::cuboid(0.5, 0.5, 0.5),
         PhysicsDynamicRigidBodyComponent {}
     ));
@@ -48,16 +75,55 @@ fn interpolate_entities_system(
     bodies: ResMut<RigidBodySet>
 ) {
 
-    for (rigid_body_handle, traitDynamicRigidBody) in query.iter_mut() {
-
+    for (rigid_body_handle, _trait_dynamic_rigid_body) in query.iter() {
         
 
+        
         if let Some(rigid_body) = bodies.get(rigid_body_handle.handle()) {
             
-            info!("Falling cube is at position {} !", rigid_body.position());
+            info!("Dynamic cube is at {} !", rigid_body.position().translation);
 
         }
 
     }
+
+}
+
+const CELL_SIZE : f32 = 2.;
+const Y_CENTER_OFFSET : f32 = 1.;
+
+fn cell_id_to_world(cell_id : Vec3) -> Vec3 {
+
+    let mut world_position : Vec3 = Vec3::zero();
+
+    world_position.x = (cell_id.x * CELL_SIZE) + Y_CENTER_OFFSET;
+    world_position.y = (cell_id.y * CELL_SIZE) + Y_CENTER_OFFSET;
+    world_position.z = (cell_id.z * CELL_SIZE) + Y_CENTER_OFFSET;
+
+    return world_position;
+
+}
+
+const STRING_VEC3_TO_VEC3_CANNOT_PARSE_MESSAGE : &str = "main.rs json_vec3_to_vec3() Error cannot parse cell id string as Vector 3.";
+
+fn json_vec3_to_vec3(string_vector : &str) -> Vec3 {
+
+   
+
+    let mut split_result : Vec<&str> = string_vector.split("(").collect();
+
+    let mut new_string : &str = split_result[1];
+
+    split_result = new_string.split(")").collect();
+
+    new_string = split_result[0];
+
+    split_result = new_string.split(",").collect();
+
+    return Vec3::new(
+        split_result[0].parse::<f32>().expect(STRING_VEC3_TO_VEC3_CANNOT_PARSE_MESSAGE),
+        split_result[1].parse::<f32>().expect(STRING_VEC3_TO_VEC3_CANNOT_PARSE_MESSAGE),
+        split_result[2].parse::<f32>().expect(STRING_VEC3_TO_VEC3_CANNOT_PARSE_MESSAGE)
+    );
 
 }
