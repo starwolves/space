3f5287c2365acf6492394d10bf39be67cc1caec2
Nikita <nikitaab98@gmail.com>
2021-05-13 00:27:14 +0200

    Added reflection_probe

--- src/space_core/systems/entity_updates/reflection_probe_update.rs
+++ src/space_core/systems/entity_updates/reflection_probe_update.rs
@@ -12,6 +12,56 @@ pub fn reflection_probe_update(
         let entity_updates = entity_updates_component.updates
         .get_mut(&".".to_string()).unwrap();
 
+        entity_updates.insert(
+            "projection_enabled".to_string(),
+            EntityUpdateData::Bool(reflection_probe_component.projection_enabled)
+        );
+        entity_updates.insert(
+            "cull_mask".to_string(),
+            EntityUpdateData::Int(reflection_probe_component.cull_mask)
+        );
+        entity_updates.insert(
+            "shadows_enabled".to_string(),
+            EntityUpdateData::Bool(reflection_probe_component.shadows_enabled)
+        );
+        entity_updates.insert(
+            "extents".to_string(),
+            EntityUpdateData::Vec3(reflection_probe_component.extents)
+        );
+        entity_updates.insert(
+            "intensity".to_string(),
+            EntityUpdateData::Float(reflection_probe_component.intensity)
+        );
+        entity_updates.insert(
+            "interior_ambient".to_string(),
+            EntityUpdateData::Color(reflection_probe_component.interior_ambient)
+        );
+        entity_updates.insert(
+            "interior_ambient_probe_contribution".to_string(),
+            EntityUpdateData::Float(reflection_probe_component.interior_ambient_probe_contribution)
+        );
+        entity_updates.insert(
+            "interior_ambient_energy".to_string(),
+            EntityUpdateData::Float(reflection_probe_component.interior_ambient_energy)
+        );
+        entity_updates.insert(
+            "set_as_interior".to_string(),
+            EntityUpdateData::Bool(reflection_probe_component.set_as_interior)
+        );
+        entity_updates.insert(
+            "max_distance".to_string(),
+            EntityUpdateData::Float(reflection_probe_component.max_distance)
+        );
+        entity_updates.insert(
+            "origin_offset".to_string(),
+            EntityUpdateData::Vec3(reflection_probe_component.origin_offset)
+        );
+        entity_updates.insert(
+            "update_mode".to_string(),
+            EntityUpdateData::Int(reflection_probe_component.update_mode as i64)
+        );
+        
+
     }
 
 
