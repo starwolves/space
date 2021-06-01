9b04adbcd9c3843579849e5883e369fea98230c7
Nikita <nikitaab98@gmail.com>
2021-06-01 14:39:03 +0200

    Expanded sensable component

--- src/space_core/components/sensable.rs
+++ src/space_core/components/sensable.rs
@@ -6,7 +6,8 @@ pub struct Sensable{
     pub is_light : bool,
     pub is_audible : bool,
     pub sensed_by : Vec<Entity>,
-    pub sensed_by_cached : Vec<Entity>
+    pub sensed_by_cached : Vec<Entity>,
+    pub always_sensed : bool
 }
 
 
