73e719241f5914291a71e08b66d1e298077040d8
Nikita <nikitaab98@gmail.com>
2021-06-30 20:45:59 +0200

    Improved the main game loop.

--- src/space_core/systems/general/done_boarding.rs
+++ src/space_core/systems/general/done_boarding.rs
@@ -1,25 +1,24 @@
-use bevy::{core::Timer, prelude::{Commands,  EventWriter, Query, RemovedComponents, ResMut, info}};
+use bevy::{core::Timer, prelude::{Commands, EventReader, EventWriter, ResMut, info}};
 
-use crate::space_core::{components::{boarding::Boarding, connected_player::ConnectedPlayer, on_board::OnBoard, persistent_player_data::PersistentPlayerData, setup_phase::SetupPhase, soft_player::SoftPlayer, spawning::Spawning}, events::net::{net_done_boarding::NetDoneBoarding}, resources::{asana_boarding_announcements::AsanaBoardingAnnouncements,  spawn_points::SpawnPoints}, structs::network_messages::{ReliableServerMessage,ServerConfigMessage}};
+use crate::space_core::{components::{ on_board::OnBoard,  setup_phase::SetupPhase, soft_player::SoftPlayer, spawning::Spawning}, events::{general::boarding_player::BoardingPlayer, net::{net_done_boarding::NetDoneBoarding}}, resources::{asana_boarding_announcements::AsanaBoardingAnnouncements,  spawn_points::SpawnPoints}, structs::network_messages::{ReliableServerMessage,ServerConfigMessage}};
 
 pub fn done_boarding(
     mut spawn_points : ResMut<SpawnPoints>,
-    players_done_boarding: RemovedComponents<Boarding>,
-    query : Query<(&SetupPhase, &ConnectedPlayer, &PersistentPlayerData)>,
     mut net_done_boarding: EventWriter<NetDoneBoarding>,
+    mut boarding_player_event : EventReader<BoardingPlayer>,
     mut commands : Commands,
 
 
     mut asana_boarding_announcements : ResMut<AsanaBoardingAnnouncements>
 ) {
 
-    for entity_id in players_done_boarding.iter() {
+    for boarding_player in boarding_player_event.iter() {
 
-        let (_setup_phase, connected_player, persistent_player_data) = 
-        query.get(entity_id)
-        .expect("done_boarding.rs could not find components for player that just got done boarding.");
+        let player_character_name = boarding_player.player_character_name.clone();
+        let player_handle = boarding_player.player_handle;
+        let entity_id = boarding_player.entity;
 
-        info!("{} [{}] has boarded the spaceship.",persistent_player_data.character_name, connected_player.handle);
+        info!("{} [{}] has boarded the spaceship.",player_character_name, player_handle);
 
         let assigned_spawn_transform = spawn_points.list[spawn_points.i].transform;
 
@@ -35,12 +34,12 @@ pub fn done_boarding(
 
         // Queue net_code message for client so he goes back to the main scene and ditches setupUI.
         net_done_boarding.send(NetDoneBoarding {
-            handle : connected_player.handle,
+            handle : player_handle,
             message: ReliableServerMessage::ConfigMessage(ServerConfigMessage::ChangeScene(true, "main".to_string()))
         });
 
         asana_boarding_announcements.announcements.insert(
-            ";Security Officer ".to_owned() + &persistent_player_data.character_name + " is now on board.", 
+            ";Security Officer ".to_owned() + &player_character_name + " is now on board.", 
             Timer::from_seconds(2., false)
         );
 
--- src/space_core/systems/general/ui_input_transmit_data_event.rs
+++ src/space_core/systems/general/ui_input_transmit_data_event.rs
@@ -1,12 +1,13 @@
-use bevy::prelude::{Commands, EventReader, Query, Res, ResMut};
+use bevy::prelude::{Commands, EventReader, EventWriter, Query, Res, ResMut};
 
-use crate::space_core::{components::{boarding::Boarding, persistent_player_data::PersistentPlayerData}, events::general::ui_input_transmit_text::UIInputTransmitText, functions::new_chat_message::escape_bb, resources::{handle_to_entity::HandleToEntity, used_names::UsedNames}};
+use crate::space_core::{components::{boarding::Boarding, connected_player::ConnectedPlayer, persistent_player_data::PersistentPlayerData}, events::general::{boarding_player::BoardingPlayer, ui_input_transmit_text::UIInputTransmitText}, functions::new_chat_message::escape_bb, resources::{handle_to_entity::HandleToEntity, used_names::UsedNames}};
 
 pub fn ui_input_transmit_data_event(
     mut event : EventReader<UIInputTransmitText>,
+    mut boarding_player_event : EventWriter<BoardingPlayer>,
     handle_to_entity: Res<HandleToEntity>,
     mut used_names : ResMut<UsedNames>,
-    mut query : Query<&mut PersistentPlayerData>,
+    mut query : Query<(&mut PersistentPlayerData, &ConnectedPlayer)>,
     mut commands : Commands
 ) {
 
@@ -16,8 +17,11 @@ pub fn ui_input_transmit_data_event(
         let player_entity = handle_to_entity.map.get(&new_event.handle)
         .expect("ui_input_transmit_text_event.rs could not find entity belonging to player handle.");
 
-        let mut persistent_player_data = query.get_mut(*player_entity)
+        let player_components = query.get_mut(*player_entity)
         .expect("ui_input_transmit_text_event.rs could not find components belonging to player.");
+
+        let mut persistent_player_data = player_components.0;
+        let connected_player_component = player_components.1;
 
         if new_event.ui_type == "setupUI" {
 
@@ -37,7 +41,13 @@ pub fn ui_input_transmit_data_event(
 
                 used_names.names.push(persistent_player_data.character_name.clone());
 
-                commands.entity(*player_entity).remove::<Boarding>();  
+                commands.entity(*player_entity).remove::<Boarding>();
+
+                boarding_player_event.send(BoardingPlayer{
+                    entity: *player_entity,
+                    player_handle: connected_player_component.handle,
+                    player_character_name: persistent_player_data.character_name.clone(),
+                });
     
             }
     
