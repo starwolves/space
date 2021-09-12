6ee1bbd30b5e0183175d380338ed16241d0474c3
Nikita <nikitaab98@gmail.com>
2021-09-12 02:42:01 +0200

    Added precise selector when encapsulating with quotes.

--- src/space_core/functions/console_commands/player_selector_to_entities.rs
+++ src/space_core/functions/console_commands/player_selector_to_entities.rs
@@ -5,10 +5,12 @@ use crate::space_core::{events::net::net_console_commands::NetConsoleCommands, r
 pub fn player_selector_to_entities(
     command_executor_entity : Entity,
     command_executor_handle : u32,
-    player_selector : &str,
+    player_selector_input : &str,
     used_names : &mut ResMut<UsedNames>,
     net_console_commands : &mut EventWriter<NetConsoleCommands>,
 ) -> Vec<Entity> {
+
+    let mut player_selector = player_selector_input.clone().to_string();
 
     let mut target_entities = vec![];
 
@@ -21,18 +23,38 @@ pub fn player_selector_to_entities(
     } else if player_selector == "@me" {
 
         target_entities.push(command_executor_entity);
+        
 
     } else {
 
         // Assume we only target one player.
 
+        
+
         let mut found_one_match = false;
         let mut conflicting_names = vec![];
 
+        let precise_match;
+
+        if (player_selector.starts_with("\"") ||  player_selector.starts_with("\'"))
+         &&  (player_selector.ends_with("\"") ||  player_selector.ends_with("\'")) {
+            precise_match=true;
+            player_selector = player_selector.replace("\"", "");
+        } else {
+            precise_match = false;
+        }
+
         for (player_name, entity) in used_names.names.iter() {
 
-            if player_name.to_lowercase().contains(&player_selector.to_lowercase()) {
+            let matcher;
 
+            if precise_match == false {
+                matcher = player_name.to_lowercase().contains(&player_selector.to_lowercase());
+            } else {
+                matcher = player_name.to_lowercase() == player_selector.to_lowercase();
+            }
+
+            if matcher {
                 if found_one_match {
                     found_one_match=false;
                     conflicting_names.push((player_name, entity));
@@ -40,7 +62,6 @@ pub fn player_selector_to_entities(
                     found_one_match=true;
                     conflicting_names.push((player_name, entity));
                 }
-
             }
 
         }
@@ -51,7 +72,7 @@ pub fn player_selector_to_entities(
 
         } else {
 
-            let mut conflicting_message = "[color=#ff6600]Player selector \"".to_string() + player_selector + " \" is not specific enough:\n";
+            let mut conflicting_message = "[color=#ff6600]Player selector \"".to_string() + &player_selector + " \" is not specific enough:\n";
 
             for (name, _entity) in conflicting_names.iter() {
 
