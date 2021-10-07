43129fa14b2667c889da0a7572d017d6f17aa930
Nikita <nikitaab98@gmail.com>
2021-10-07 22:41:18 +0200

    Added pistol combat preparations

--- src/space_core/bundles/pistol_l1.rs
+++ src/space_core/bundles/pistol_l1.rs
@@ -1,9 +1,9 @@
 use std::collections::{BTreeMap, HashMap};
 
-use bevy::{math::{Mat4, Quat, Vec3}, prelude::{Commands, Entity, EventWriter, Transform, warn}};
+use bevy::{math::{Mat4, Quat, Vec3}, prelude::{Color, Commands, Entity, EventWriter, Transform, warn}};
 use bevy_rapier3d::prelude::{CoefficientCombineRule, ColliderBundle, ColliderFlags, ColliderMaterial, ColliderPosition, ColliderShape, InteractionGroups, RigidBodyActivation, RigidBodyBundle, RigidBodyCcd, RigidBodyForces, RigidBodyType};
 
-use crate::space_core::{components::{cached_broadcast_transform::CachedBroadcastTransform, default_transform::DefaultTransform, entity_data::{EntityData}, entity_updates::EntityUpdates, examinable::Examinable, health::{DamageFlag, DamageModel, Health}, interpolation_priority::{InterpolationPriority}, inventory::SlotType, inventory_item::{CombatAttackAnimation, CombatStandardAnimation, CombatType, InventoryItem, MeleeCombatSoundSet}, pistol_l1::PistolL1, rigidbody_disabled::RigidBodyDisabled, rigidbody_link_transform::RigidBodyLinkTransform, sensable::Sensable, showcase::Showcase, world_mode::{WorldMode, WorldModes}}, events::net::net_showcase::NetShowcase, functions::{converters::transform_to_isometry::transform_to_isometry, entity::{collider_interaction_groups::{ColliderGroup, get_bit_masks}}}, resources::network_messages::ReliableServerMessage};
+use crate::space_core::{components::{cached_broadcast_transform::CachedBroadcastTransform, default_transform::DefaultTransform, entity_data::{EntityData}, entity_updates::EntityUpdates, examinable::Examinable, health::{DamageFlag, DamageModel, Health}, interpolation_priority::{InterpolationPriority}, inventory::SlotType, inventory_item::{CombatAttackAnimation, CombatStandardAnimation, CombatType, InventoryItem, MeleeCombatSoundSet, ProjectileType}, pistol_l1::PistolL1, rigidbody_disabled::RigidBodyDisabled, rigidbody_link_transform::RigidBodyLinkTransform, sensable::Sensable, showcase::Showcase, world_mode::{WorldMode, WorldModes}}, events::net::net_showcase::NetShowcase, functions::{converters::transform_to_isometry::transform_to_isometry, entity::{collider_interaction_groups::{ColliderGroup, get_bit_masks}}}, resources::network_messages::ReliableServerMessage};
 
 use super::helmet_security::STANDARD_BODY_FRICTION;
 
@@ -233,7 +233,7 @@ Vec3::new(0.,0.355, 0.)
             slot_type: SlotType::Generic,
             is_attached_when_worn : true,
             combat_attack_animation : CombatAttackAnimation::PistolShot,
-            combat_type: CombatType::Projectile,
+            combat_type: CombatType::Projectile(ProjectileType::Laser(Color::RED, 0.14, 0.025)),
             combat_damage_model : DamageModel {
                 brute: 9.,
                 damage_flags: first_damage_flags,
