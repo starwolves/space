ea7acf06abf3c4566cd6c63fb4aa79e019903dbd
Ramses <nikitaab98@gmail.com>
2022-02-12 03:27:28 +0100

    Improved atmospherics.

--- src/space_core/ecs/atmospherics/systems/effects.rs
+++ src/space_core/ecs/atmospherics/systems/effects.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::ResMut;
 
-use crate::space_core::ecs::atmospherics::resources::AtmosphericsResource;
+use crate::space_core::ecs::atmospherics::resources::{AtmosphericsResource, CELCIUS_KELVIN_OFFSET};
+
+const ATMOS_EFFECT_SPEED : f32 = 0.01;
 
 pub fn atmos_effects(
     mut atmospherics_resource : ResMut<AtmosphericsResource>,
@@ -13,14 +15,39 @@ pub fn atmos_effects(
 
         for effect in atmospherics.effects.values() {
 
-            total_amount_additive += effect.amount_additive;
-            total_temperature_additive += effect.temperature_additive;
+            if !effect.heater {
+                if atmospherics.temperature > effect.target_temperature {
+                    total_temperature_additive -= effect.target_temperature * effect.temperature_speed;
+                }
+            } else {
+                if atmospherics.temperature < effect.target_temperature {
+                    total_temperature_additive += effect.target_temperature * effect.temperature_speed;
+                }
+            }
 
+            if effect.remover {
+                if atmospherics.amount > effect.target_amount {
+                    total_amount_additive -= effect.target_amount * effect.amount_speed;
+                }
+            } else {
+                if atmospherics.amount < effect.target_amount {
+                    total_amount_additive += effect.target_amount * effect.amount_speed;
+                }
+            }
+            
         }
 
-        atmospherics.amount += total_amount_additive * 0.01;
-        atmospherics.temperature += total_temperature_additive * 0.01;
+        atmospherics.amount += total_amount_additive * ATMOS_EFFECT_SPEED;
+        atmospherics.temperature += total_temperature_additive * ATMOS_EFFECT_SPEED;
 
+
+        if atmospherics.amount < 0. {
+            atmospherics.amount = 0.;
+        }
+
+        if atmospherics.temperature < -270.45 + CELCIUS_KELVIN_OFFSET {
+            atmospherics.temperature = -270.45 + CELCIUS_KELVIN_OFFSET;
+        }
 
     }
 
