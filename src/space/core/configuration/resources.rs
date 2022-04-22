15ae720ef2f74a68a0e42b09ed2e8108670338f6
Rαmses <contact@starwolves.io>
2022-04-22 06:47:23 +0200

    Improved default MOTD message.

--- src/space/core/configuration/resources.rs
+++ src/space/core/configuration/resources.rs
@@ -17,11 +17,11 @@ impl FromWorld for MOTD {
     fn from_world(_world: &mut World) -> Self {
         MOTD {
             message :  "[center]".to_string() +
-            "[font=" + NEARBY_SHOUT_FONT + "][color=" + COMMUNITY_HREF_COLOR + "][url={\"type\": \"href\",\"data\":\"https://starwolves.io\"}]STARWOLVES.IO[/url][/color][/font]\n" +
+            "[font=" + NEARBY_SHOUT_FONT + "][color=" + COMMUNITY_HREF_COLOR + "][url={\"type\": \"href\",\"data\":\"https://github.com/starwolves/space\"}]Space Frontiers[/url][/color][/font]\n" +
             "Welcome to the official test server of Space Frontiers. (v" + VERSION + ")\n\n" +
             "You are about to board The Bullseye, a research & development ship currently occupied by the Red Sun Nation.\n\n" +
-            "The Star Wolves gaming community is thrilled to have you here, you are invited to connect with our new gaming community through our social platforms!\n" + 
-            "[font=" + NEARBY_SHOUT_FONT + "][color=" + COMMUNITY_HREF_COLOR + "][url={\"type\": \"href\",\"data\":\"https://starwolves.io\"}]StarWolves.io[/url][/color][/font]\n" +
+            "The Space Frontiers community is thrilled to have you here, you are invited to connect with our new gaming community through our social platforms!\n" + 
+            "[font=" + NEARBY_SHOUT_FONT + "][color=" + COMMUNITY_HREF_COLOR + "][url={\"type\": \"href\",\"data\":\"https://github.com/starwolves/space\"}]Github[/url][/color][/font]\n" +
             "[/center]",
         }
     }
