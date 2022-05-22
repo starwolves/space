c96e04eb6d184b6303848cba5936127c3b29d3b9
Rαmses <contact@starwolves.io>
2022-05-22 19:14:17 +0200

    0.7.0 integration

--- src/core/rigid_body/mod.rs
+++ src/core/rigid_body/mod.rs
@@ -1,3 +1,19 @@
+use bevy_app::{App, CoreStage, Plugin};
+
+use self::systems::{
+    broadcast_interpolation_transforms::broadcast_interpolation_transforms,
+    out_of_bounds_check::out_of_bounds_check, rigidbody_link_transform::rigidbody_link_transform,
+};
+
 pub mod components;
 pub mod functions;
 pub mod systems;
+
+pub struct RigidBodyPlugin;
+impl Plugin for RigidBodyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system(out_of_bounds_check)
+            .add_system_to_stage(CoreStage::Update, broadcast_interpolation_transforms)
+            .add_system(rigidbody_link_transform);
+    }
+}
--- src/core/rigid_body/systems/mod.rs
+++ src/core/rigid_body/systems/mod.rs
@@ -1,17 +1,3 @@
-use bevy_app::{App, CoreStage, Plugin};
-
 pub mod broadcast_interpolation_transforms;
 pub mod out_of_bounds_check;
 pub mod rigidbody_link_transform;
-
-use broadcast_interpolation_transforms::broadcast_interpolation_transforms;
-use out_of_bounds_check::out_of_bounds_check;
-
-pub struct RigidBodyPlugin;
-
-impl Plugin for RigidBodyPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_system(out_of_bounds_check)
-            .add_system_to_stage(CoreStage::Update, broadcast_interpolation_transforms);
-    }
-}
