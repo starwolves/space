13f8b56e047a7dc75d715bb14f0a89c564eaa333
Rαmses <contact@starwolves.io>
2022-05-24 01:03:57 +0200

    Merged and disabled AI from 0.0.3-snap3 .

--- src/core/artificial_unintelligence/mod.rs
+++ src/core/artificial_unintelligence/mod.rs
@@ -1,25 +1,21 @@
 use bevy_app::{App, Plugin};
-use bevy_ecs::schedule::ParallelSystemDescriptorCoercion;
 
-use self::{
+/*use self::{
     resources::ContextMapVectors,
-    systems::{find_path::find_path, spawn_ai::spawn_ai, steer::steer},
-};
-
-use super::StartupLabels;
+    systems::{find_path::find_path, steer::steer},
+};*/
 
 pub mod components;
 pub mod functions;
-mod resources;
-pub mod systems;
+pub mod resources;
+//pub mod systems;
 
 pub struct ArtificialUnintelligencePlugin;
 
 impl Plugin for ArtificialUnintelligencePlugin {
-    fn build(&self, app: &mut App) {
-        app.init_resource::<ContextMapVectors>()
-            .add_startup_system(spawn_ai.after(StartupLabels::InitDefaultGridmapData))
-            .add_system(find_path)
-            .add_system(steer);
+    fn build(&self, _app: &mut App) {
+       // app.init_resource::<ContextMapVectors>()
+         //   .add_system(find_path)
+        //    .add_system(steer);
     }
 }
