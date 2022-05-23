42a1584cfa3395bedfe54f6a4b8ed8a6160f2cf7
Rαmses <contact@starwolves.io>
2022-05-24 00:57:08 +0200

    Fixed warnings.

--- src/core/artificial_unintelligence/systems/steer.rs
+++ src/core/artificial_unintelligence/systems/steer.rs
@@ -43,8 +43,8 @@ pub fn steer(
             Vec2::new(1., 0.),
             Vec2::new(0.70710677, -0.70710677),
         ];
-        let mut interest_map: [i32; CONTEXT_MAP_RESOLUTION] = [-1; CONTEXT_MAP_RESOLUTION];
-        let mut danger_map: [i32; CONTEXT_MAP_RESOLUTION] = [-1; CONTEXT_MAP_RESOLUTION];
+        let mut _interest_map: [i32; CONTEXT_MAP_RESOLUTION] = [-1; CONTEXT_MAP_RESOLUTION];
+        let mut _danger_map: [i32; CONTEXT_MAP_RESOLUTION] = [-1; CONTEXT_MAP_RESOLUTION];
         let mut waypoints: [Option<Vec3>; 8] = [None; 8];
         let danger_waypoints: [Option<Vec3>; 8];
         let mut _chosen_vector = Vec2::ZERO;
@@ -61,16 +61,16 @@ pub fn steer(
                 &gridmap.grid_data,
                 &gridmap_data.main_id_name_map,
             );
-            interest_map =
+            _interest_map =
                 create_context_map(waypoints, current_location, goal.action, mapped_vectors);
-            danger_map = create_context_map(
+            _danger_map = create_context_map(
                 danger_waypoints,
                 current_location,
                 goal.action,
                 mapped_vectors,
             );
 
-            _chosen_vector = choose_vector(interest_map, danger_map, mapped_vectors);
+            _chosen_vector = choose_vector(_interest_map, _danger_map, mapped_vectors);
             controller.movement_vector = get_vector(waypoints[0].unwrap(), current_location);
         } else {
             controller.movement_vector = Vec2::new(0., 0.);
@@ -78,7 +78,7 @@ pub fn steer(
             _chosen_vector = Vec2::new(-10., -10.);
         }
 
-        let old_position = blob.temp_position;
+        let _old_position = blob.temp_position;
         blob.temp_position = world_to_cell_id(transform.transform.translation);
 
         //Never fires.
