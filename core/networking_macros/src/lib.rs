ee682880f84bab07ae9227db6a7b25d4f7c236e2
starwolf <ramses@starwolves.io>
2022-09-28 19:34:31 +0200

    Improved code privacy and documentation.

--- core/networking_macros/src/lib.rs
+++ core/networking_macros/src/lib.rs
@@ -2,6 +2,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 
+/// The derivation for netcode messages.
 #[proc_macro_derive(NetMessage)]
 pub fn derive_net_message(item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
