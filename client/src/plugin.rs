2d7cd6c5e485463c255ddec37fcb4ca28dd714c7
starwolf <ramses@starwolves.io>
2022-11-15 01:47:06 +0100

    Improved client window and main menu.

--- client/src/plugin.rs
+++ client/src/plugin.rs
@@ -3,10 +3,12 @@ use std::time::Duration;
 use bevy::{
     app::{RunMode, ScheduleRunnerSettings},
     prelude::{App, DefaultTaskPoolOptions, Plugin},
+    window::{MonitorSelection, PresentMode, WindowDescriptor, WindowMode, WindowPosition},
     winit::WinitSettings,
     DefaultPlugins,
 };
 use menu_main::plugin::MainMenuPlugin;
+use winit_windows::plugin::WinitWindowsPlugin;
 
 use crate::client_is_live;
 
@@ -33,15 +35,26 @@ impl Plugin for ClientPlugin {
             },
             encryption_key_string,
         ))*/
-        app.add_plugins(DefaultPlugins)
-            .insert_resource(WinitSettings::game())
-            .add_plugin(MainMenuPlugin)
-            .add_startup_system(client_is_live)
-            .insert_resource(ScheduleRunnerSettings {
-                run_mode: RunMode::Loop {
-                    wait: Some(Duration::from_secs_f64(1. / (64. as f64))),
-                },
-            });
+        app.insert_resource(WindowDescriptor {
+            title: "Space Frontiers ".to_string() + &self.version,
+            width: 1280.,
+            height: 720.,
+            present_mode: PresentMode::AutoNoVsync,
+            position: WindowPosition::Centered(MonitorSelection::Primary),
+            mode: WindowMode::Windowed,
+            transparent: true,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .insert_resource(WinitSettings::game())
+        .add_plugin(MainMenuPlugin)
+        .add_plugin(WinitWindowsPlugin)
+        .add_startup_system(client_is_live)
+        .insert_resource(ScheduleRunnerSettings {
+            run_mode: RunMode::Loop {
+                wait: Some(Duration::from_secs_f64(1. / (64. as f64))),
+            },
+        });
         match self.threads_amount {
             Some(amn) => {
                 app.insert_resource(DefaultTaskPoolOptions::with_num_threads(amn.into()));
---
+++ core/winit_windows/src/lib.rs
@@ -0,0 +1,6 @@
+//! Sets WinitWindows settings.
+
+// The Bevy plugin of this crate.
+pub mod plugin;
+// Set window icon of client.
+pub mod set_icon;
