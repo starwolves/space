5d26443cdc01f6e852416f4603592662521342e4
starwolf <ramses@starwolves.io>
2022-12-12 07:48:46 +0100

    Abolished gridmap_meta crate.

--- entities/counter_windows/src/plugin.rs
+++ entities/counter_windows/src/plugin.rs
@@ -5,12 +5,12 @@ use bevy::{
     prelude::{App, IntoSystemDescriptor, Plugin, ResMut, SystemSet, Transform},
 };
 use combat::sfx::health_combat_hit_result_sfx;
+use entity::meta::GridItemData;
 use entity::{
     entity_data::initialize_entity_data,
     meta::{EntityDataProperties, EntityDataResource},
     spawn::{summon_base_entity, SpawnEvent},
 };
-use gridmap_meta::core::GridItemData;
 use physics::spawn::summon_rigid_body;
 use resources::labels::{
     ActionsLabels, CombatLabels, PostUpdateLabels, StartupLabels, SummoningLabels,
