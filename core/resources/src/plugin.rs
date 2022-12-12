7d3e4ba340d496298e2f64c8b2d57a02fa6034b7
starwolf <ramses@starwolves.io>
2022-12-12 08:05:55 +0100

    Abolished winit_windows crate.

--- core/resources/src/plugin.rs
+++ core/resources/src/plugin.rs
@@ -2,7 +2,7 @@ use std::env;
 
 use bevy::prelude::{App, Plugin};
 
-use crate::core::ServerId;
+use crate::{core::ServerId, set_icon::set_window_icon};
 
 pub struct ResourcesPlugin;
 
@@ -10,7 +10,8 @@ impl Plugin for ResourcesPlugin {
     fn build(&self, app: &mut App) {
         if env::var("CARGO_MANIFEST_DIR").unwrap().ends_with("server") {
             app.init_resource::<ServerId>();
+        } else {
+            app.add_startup_system(set_window_icon);
         }
-        if cfg!(feature = "client") {}
     }
 }
--- core/winit_windows/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Sets WinitWindows settings.
-
-// The Bevy plugin of this crate.
-pub mod plugin;
-// Set window icon of client.
-pub mod set_icon;
