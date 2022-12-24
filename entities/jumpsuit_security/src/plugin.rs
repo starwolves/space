d48d31ed47d46f686cfd820f03114e1152a2467f
starwolf <ramses@starwolves.io>
2022-12-24 10:32:44 +0100

    Generalized content init startup systems.

--- entities/jumpsuit_security/src/plugin.rs
+++ entities/jumpsuit_security/src/plugin.rs
@@ -1,19 +1,14 @@
-use bevy::prelude::{App, IntoSystemDescriptor, Plugin, ResMut};
+use bevy::prelude::{App, IntoSystemDescriptor, Plugin};
 use combat::{
     melee_queries::melee_attack_handler,
     sfx::{attack_sfx, health_combat_hit_result_sfx},
 };
-use entity::{
-    entity_data::initialize_entity_data,
-    entity_types::init_entity_type,
-    meta::{EntityDataProperties, EntityDataResource},
-    spawn::build_base_entities,
-};
+use entity::{entity_types::init_entity_type, spawn::build_base_entities};
 use inventory::spawn_item::build_inventory_items;
 use physics::spawn::build_rigid_bodies;
 use resources::{
     is_server::is_server,
-    labels::{BuildingLabels, CombatLabels, StartupLabels},
+    labels::{BuildingLabels, CombatLabels},
 };
 
 use crate::jumpsuit::Jumpsuit;
@@ -40,8 +35,7 @@ impl Plugin for JumpsuitsPlugin {
             );
         }
         init_entity_type::<JumpsuitType>(app);
-        app.add_startup_system(content_initialization.before(StartupLabels::InitEntities))
-            .add_system(build_jumpsuits::<JumpsuitType>.after(BuildingLabels::TriggerBuild))
+        app.add_system(build_jumpsuits::<JumpsuitType>.after(BuildingLabels::TriggerBuild))
             .add_system((build_base_entities::<JumpsuitType>).after(BuildingLabels::TriggerBuild))
             .add_system((build_rigid_bodies::<JumpsuitType>).after(BuildingLabels::TriggerBuild))
             .add_system((build_raw_jumpsuits).after(BuildingLabels::TriggerBuild))
@@ -52,14 +46,4 @@ impl Plugin for JumpsuitsPlugin {
                     .after(BuildingLabels::NormalBuild),
             );
     }
-}
-
-pub fn content_initialization(mut entity_data: ResMut<EntityDataResource>) {
-    let entity_properties = EntityDataProperties {
-        name: JumpsuitType::default().identifier,
-        id: entity_data.get_id_inc(),
-        ..Default::default()
-    };
-
-    initialize_entity_data(&mut entity_data, entity_properties);
 }
