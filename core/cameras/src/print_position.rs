4187ddc9b68fddc0ce1f131f3c75a99123f8ec36
starwolf <ramses@starwolves.io>
2023-06-24 16:32:24 +0200

    Add more lights to map.

--- core/cameras/src/print_position.rs
+++ core/cameras/src/print_position.rs
@@ -3,7 +3,7 @@ use bevy::prelude::{Query, Transform, With};
 use crate::controllers::fps::FpsCameraController;
 
 #[allow(dead_code)]
-fn print_camera_position(cameras: Query<&Transform, With<FpsCameraController>>) {
+pub fn print_camera_position(cameras: Query<&Transform, With<FpsCameraController>>) {
     for cam in cameras.iter() {
         bevy::log::info!("{:?}", cam.translation);
     }
