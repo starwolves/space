65ea03e15020d0bf91edd4a585bb386549517ef1
starwolf <ramses@starwolves.io>
2023-12-14 16:34:03 +0100

    Limit unreliable mouse input frequency to 60hz.

--- core/pawn/src/plugin.rs
+++ core/pawn/src/plugin.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use crate::actions::{build_actions, examine, examine_prerequisite_check};
 use crate::camera::{
     clear_mouse_stamps, clear_mouse_stamps_server, client_sync_look_transform,
@@ -7,6 +9,7 @@ use crate::net::UnreliableControllerClientMessage;
 use bevy::app::Update;
 use bevy::ecs::schedule::common_conditions::resource_exists;
 use bevy::prelude::{App, FixedUpdate, IntoSystemConfigs, Plugin};
+use bevy::time::common_conditions::on_timer;
 use bevy_renet::renet::RenetClient;
 use networking::messaging::{register_unreliable_message, MessageSender};
 use resources::modes::is_server_mode;
@@ -43,7 +46,8 @@ impl Plugin for PawnPlugin {
                 client_sync_look_transform
                     .run_if(resource_exists::<RenetClient>())
                     .in_set(MainSet::Update)
-                    .in_set(LookTransformSet::Sync),
+                    .in_set(LookTransformSet::Sync)
+                    .run_if(on_timer(Duration::from_secs_f32(1. / 60.))),
             )
             .init_resource::<MouseInputStamps>()
             .add_systems(
